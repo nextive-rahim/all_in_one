6d90c52f1dcba2e7132af3c23b3df31b1a25e564
Abdul Rahim <rahim.nextive@gmail.com>
2024-08-21 04:08:01 +0600

    refactor :Refacator delete employee button.

---
+++ lib/src/features/company_module/mobile/manage_and_add_courses_employees/employee_list/widget/delete_employee_button.dart
@@ -0,0 +1,74 @@
+import 'package:all_in_one/src/core/utils/colors.dart';
+import 'package:all_in_one/src/features/company_module/mobile/manage_and_add_courses_employees/employee_list/controller/employee_view_controller.dart';
+import 'package:all_in_one/src/features/company_module/mobile/manage_and_add_courses_employees/employee_list/model/employee_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DeleteEmployeeButton extends GetView<EmployeeListViewController> {
+  const DeleteEmployeeButton({
+    super.key,
+    required this.employee,
+  });
+  final EmployeeModel employee;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _showDailogBox(context);
+      },
+      child: const Icon(
+        Icons.delete_forever_outlined,
+        color: CommonColor.redColors,
+        size: 28,
+      ),
+    );
+  }
+
+  void _showDailogBox(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Do you want to delete this employee.'),
+          content: const SingleChildScrollView(
+            child: SelectableText(
+              'Delete is irreversible.',
+              style: TextStyle(color: CommonColor.redColors),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                controller.deleteEmployee(employee.userId ?? 0).then((v) {
+                  if (v.success == true) {
+                    controller.employeeList
+                        .removeWhere((v) => v.id == employee.id);
+                  } else {
+                    Get.snackbar(
+                      'Failded',
+                      v.message ?? '',
+                      backgroundColor: CommonColor.redColors,
+                      colorText: CommonColor.whiteColor,
+                    );
+                  }
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                'OK',
+                style: TextStyle(color: CommonColor.redColors),
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
--- lib/src/features/company_module/mobile/manage_and_add_courses_employees/employee_list/widget/employee_card.dart
+++ lib/src/features/company_module/mobile/manage_and_add_courses_employees/employee_list/widget/employee_card.dart
@@ -4,8 +4,8 @@ import 'package:all_in_one/src/core/theme/text_style.dart';
 import 'package:all_in_one/src/core/utils/colors.dart';
 import 'package:all_in_one/src/core/utils/size_config.dart';
 import 'package:all_in_one/src/core/utils/string.dart';
-import 'package:all_in_one/src/features/company_module/mobile/manage_and_add_courses_employees/employee_list/controller/employee_view_controller.dart';
 import 'package:all_in_one/src/features/company_module/mobile/manage_and_add_courses_employees/employee_list/model/employee_model.dart';
+import 'package:all_in_one/src/features/company_module/mobile/manage_and_add_courses_employees/employee_list/widget/delete_employee_button.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -121,16 +121,7 @@ class EmployeeCard extends StatelessWidget {
                         ],
                       ),
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        _showDailogBox(context);
-                      },
-                      child: const Icon(
-                        Icons.delete_forever_outlined,
-                        color: CommonColor.redColors,
-                        size: 28,
-                      ),
-                    )
+                    DeleteEmployeeButton(employee: employee)
                   ],
                 ),
               ),
@@ -209,56 +200,6 @@ class EmployeeCard extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  void _showDailogBox(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Do you want to delete this employee.'),
-          content: const SingleChildScrollView(
-            child: SelectableText(
-              'Delete is irreversible.',
-              style: TextStyle(color: CommonColor.redColors),
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                Get.find<EmployeeListViewController>()
-                    .deleteEmployee(employee.id ?? 0)
-                    .then((v) {
-                  if (v.success == true) {
-                    Get.find<EmployeeListViewController>()
-                        .employeeList
-                        .removeWhere((v) => v.id == employee.id);
-                  } else {
-                    Get.snackbar(
-                      'Failded',
-                      v.message ?? '',
-                      backgroundColor: CommonColor.redColors,
-                      colorText: CommonColor.whiteColor,
-                    );
-                  }
-                });
-                Navigator.of(context).pop();
-              },
-              child: const Text(
-                'OK',
-                style: TextStyle(color: CommonColor.redColors),
-              ),
-            )
-          ],
-        );
-      },
     );
   }
 
